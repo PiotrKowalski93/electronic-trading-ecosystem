b51e6dd23f74ff9f86a812b1e2e05da1dae1bbcd
Piotr Kowalski <piotrkow93@gmail.com>
2026-03-16 15:08:05 +0100

    Main Loop init

--- trading_system/order_gw/order_gateway.cpp
+++ trading_system/order_gw/order_gateway.cpp
@@ -4,10 +4,36 @@ namespace TradingSystem
 {   
     OrderGateway::OrderGateway(ClientId client_id, Exchange::ClientRequestLFQueue* outgoing_requests, Exchange::ClientResponseLFQueue* incoming_responses,
         std::string ip, const std::string iface, const int port): client_id_(client_id), outgoing_requests_(outgoing_requests), incoming_responses_(incoming_responses),
-            ip_(ip), iface_(iface), port_(port_), logger_("trading_order_gateway" + std::to_string(client_id) + ".log"), tcp_socket_(logger_)
+            ip_(ip), iface_(iface), port_(port_), logger_("trading_order_gateway" + std::to_string(client_id) + ".log"), tcp_client_(logger_)
     {
-        
+        tcp_client_.recv_callback_ = [this](auto socket, auto rx_time){
+            //recvCallback(socket, rx_time);
+        };
+    }
 
+    OrderGateway::~OrderGateway(){
+        stop();
+
+        using namespace std::literals::chrono_literals;
+        std::this_thread::sleep_for(5s);
+    }
+
+    auto OrderGateway::run() noexcept -> void{
+        logger_.log("%:% %() % OrderGateway::run() \n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_));
+        
+        while(is_running_){
+            tcp_client_.sendAndRecv();
+
+            for(auto request = outgoing_requests_->getNextToRead(); request; request = outgoing_requests_->getNextToRead()){
+                logger_.log("%:% %() % Sending order. ClientId: % SeqNum: % %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_), 
+                    client_id_, next_outgoing_seq_num_, request->toString());
+                
+                // Send seq Num
+                // Send req
+
+                // seq_num++
+            }
+        }
     }
 
 }
--- trading_system/order_gw/order_gateway.h
+++ trading_system/order_gw/order_gateway.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <errno.h>
+#include <cstring>
+
 #include "tcp_server.h"
 #include "common_types.h"
 #include "threads.h"
@@ -14,6 +17,8 @@ namespace TradingSystem
             OrderGateway(ClientId client_id, Exchange::ClientRequestLFQueue* outgoing_requests, Exchange::ClientResponseLFQueue* incoming_responses,
                 std::string ip, const std::string iface, const int port);
             ~OrderGateway();
+
+            auto run() noexcept -> void;
 
         private:
             const Common::ClientId client_id_;
@@ -33,6 +38,21 @@ namespace TradingSystem
 
             size_t next_outgoing_seq_num_ = 1;
             size_t next_exp_response_seq_num_ = 1;
-            Common::TCPSocket tcp_socket_;
+            Common::TCPSocket tcp_client_;
+
+            auto start() -> void {
+                is_running_ = true;
+                
+                // Connecting Gateway to the Exchange
+                ASSERT(tcp_client_.connect(ip_, iface_, port_, false) >= 0, "Unable to connect to ip: " + ip_ +
+                    " port: " + std::to_string(port_) + " iface: " + iface_ + " error: " + std::string(std::strerror(errno)));
+
+                // Running processing on separate thread
+                ASSERT(Common::createAndStartThread(-1, "Trading/OrderGateway", [this]() { run(); }) != nullptr, "Failed to start OrderGateway thread.");
+            }
+
+            auto stop() -> void {
+                is_running_ = false;
+            }
     };
 }
